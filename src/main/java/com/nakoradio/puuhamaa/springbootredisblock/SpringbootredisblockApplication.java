5849555526953693b91920aeee2c4b8eb4d9f1f7
Arch-vile <mikko.ravimo@gmail.com>
2019-04-03 08:55:37 +0300

    Bootstrapped project

---
+++ src/main/java/com/nakoradio/puuhamaa/springbootredisblock/SpringbootredisblockApplication.java
@@ -0,0 +1,13 @@
+package com.nakoradio.puuhamaa.springbootredisblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootredisblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootredisblockApplication.class, args);
+	}
+
+}
